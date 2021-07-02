d055c0364b2d31b96025be2faedd68c63e65e527
Apolycious <77037457+Apolycious@users.noreply.github.com>
2021-07-02 23:56:00 +0700

    Added new files

---
+++ assets/code/main_3.js
@@ -0,0 +1,40 @@
+setInterval(displayclock, 500); 
+function displayclock () {
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var en = 'AM';
+
+    if (hrs > 12) {
+        en = 'PM';
+    }
+
+    if (hrs > 12) {
+        hrs = hrs - 12;
+    }
+
+    if (hrs == 0) {
+        hrs = 12;
+    }
+
+    if (hrs < 10) {
+        hrs = '0' + hrs;
+    }
+
+    if (min < 10) {
+        min = '0' + min;
+    }
+    
+    document.getElementById('clock').innerHTML = hrs + ':' + min + ' ' + en;
+
+}
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 500) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
